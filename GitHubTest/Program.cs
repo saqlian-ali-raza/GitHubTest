048364dd2f0020b2f91ea68d64ae1e5b154a9ee7
saqlian-ali-raza <engr.saqlian@gmail.com>
2020-08-05 18:37:59 +0300

    Commit with 3 writeline

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -8,6 +8,7 @@ namespace GitHubTest
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
