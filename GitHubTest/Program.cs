732208725a7f0e803865a00b08f63c1b26837046
saqlian-ali-raza <engr.saqlian@gmail.com>
2020-08-05 18:35:38 +0300

    jhhbhb

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -7,6 +7,7 @@ namespace GitHubTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
